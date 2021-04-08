89ca8a17f3cbfb12756ca7dc47b773cc29f31427
Latha Murugan <81089023+LathaMurugan-code@users.noreply.github.com>
2021-04-08 21:22:57 +0530

    pattern program

---
+++ LoopPattern2.java
@@ -0,0 +1,13 @@
+public class LoopPattern2 {
+        public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int row=5;row>=1;row--)
+		{
+			for(int col=5;col>=row;col--)
+			{
+				System.out.print(row);
+			}
+			System.out.println();
+		}
+   }
+}
