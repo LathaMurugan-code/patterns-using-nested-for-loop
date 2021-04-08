bed48cbe5dcfbecf3b678003566d5cfd90c4359b
Latha Murugan <81089023+LathaMurugan-code@users.noreply.github.com>
2021-04-08 21:28:14 +0530

    Pattern

---
+++ LoopPattern6.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class LoopPattern6 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int j=5;j>=1;j--)
+{
+for(int i=1;i<=j;i++)
+{
+	System.out.print(" ");
+}
+for(int k=5;k>=j;k--)
+{
+	System.out.print("* ");
+}
+System.out.println();
+}
+   }
+}
