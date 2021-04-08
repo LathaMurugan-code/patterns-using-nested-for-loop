bb1b2bb08b106a99b10ef9839bcac5df21b0b0ef
Latha Murugan <81089023+LathaMurugan-code@users.noreply.github.com>
2021-04-08 21:24:10 +0530

    Pattern

---
+++ LoopPattern3.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class LoopPattern3 {
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int row=5;row>=1;row--)
+		{
+			for(int col=1;col<=row;col++)
+			{
+				System.out.print(col);
+			}
+			System.out.println();
+		}
+   }
+}
