fbe5db28d119c85a63bf9a21a79753666d16df13
Latha Murugan <81089023+LathaMurugan-code@users.noreply.github.com>
2021-04-11 07:56:48 +0530

    Fishmouth

---
+++ Fishmouth.java
@@ -0,0 +1,39 @@
+
+public class Sandclock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int row=1;row<=7;row++)
+		{
+			for(int col=1;col<=row;col++)
+			{
+				System.out.print(" ");
+			}
+			int a=1;
+			for(int n=7;n>=row;n--)
+			{
+			  System.out.print(a+" ");
+			  a++;
+			}
+			System.out.println();
+		}
+		for(int row=1;row<=7;row++)
+		{
+			for(int col=7;col>=row;col--)
+			{
+				System.out.print(" ");
+			}
+			int a=1;
+			for(int n=1;n<=row;n++)
+			{
+			  System.out.print(a+" ");
+			  a++;
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+
+
