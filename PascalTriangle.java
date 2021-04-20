dcf29bd9b5514fb4f521d81b4714f0a66c4a7a24
Latha Murugan <81089023+LathaMurugan-code@users.noreply.github.com>
2021-04-20 19:50:22 +0530

    pascal Triangle with Spaces

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -5,18 +5,45 @@ public class PascalTriangle {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=11,b=0;
-		for(int i=1;i<=4;i++)
+		for(int i=1;i<=7;i++)
 		{
-			for(int j=4;j>=i;j--)
+			for(int j=7;j>=i;j--)
 			{
 				System.out.print(" ");
 			}
-			System.out.print((int)Math.pow(a, b)+" ");
+			int result=(int)Math.pow(a, b);
+			//System.out.print(result);
+			digitSpliter(result);
 			b++;
 			System.out.println();
 		}
-			
-				}
+							}
+	public static void digitSpliter(int no)
+	{   int rem=0;
+	    int temp = no;
+		int count=0;
+		while(no>0)
+		{
+			 rem = no%10;
+			no=no/10;
+			count++;
+			}
+		int arr[] = new int[count];
+		for(int i=arr.length-1;i>=0;i--) {
+			rem = temp%10;
+			temp=temp/10;
+			arr[i]=rem;
+			if(temp<0)
+				break;
+		}
+		
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+	}
+
 		}
 			
 
